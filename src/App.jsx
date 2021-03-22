fa0a38ac76efdaa1b96837622654ca9eb9ba0356
Mona Lisa <patapiks@mail.ru>
2021-03-22 17:31:13 +0300

    Applied changes for mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useResizeDetector } from 'react-resize-detector';
 import Header from './components/Header';
 import Mobile from './components/Mobile';
-import PhoneInput from './components/PhoneInput';
 import Winners from './components/Winners';
 
 const App = () => {
@@ -10,7 +9,6 @@ const App = () => {
   if (width < 430) {
     return (
       <div className="container" ref={ref}>
-        <PhoneInput />
         <Mobile />;
       </div>
     );
--- src/components/Mobile.jsx
+++ src/components/Mobile.jsx
@@ -3,6 +3,9 @@ import { Carousel } from 'react-bootstrap';
 import axios from '../axios';
 import logo from '../img/JBL.png';
 
+import { useForm, Controller } from 'react-hook-form';
+import InputMask from 'react-input-mask';
+
 const Mobile = () => {
   const [state, setState] = useState({
     page: 1,
@@ -10,34 +13,65 @@ const Mobile = () => {
     prise: 'jbl_speaker',
     count: 6,
     data: [],
+    message: '',
   });
 
   useEffect(() => {
     async function loadMore() {
-      const response = await axios.get(`?page=${state.page}&prise=${state.prise}`);
+      const response = await axios.get();
       const { checks, page, total_pages } = response.data;
       setState((prev) => {
-        return { ...prev, page, total_pages, data: [...prev.data, ...checks] };
+        return { ...prev, page, total_pages, data: checks };
       });
     }
     loadMore();
   }, [state.page, state.prise]);
 
+  const { handleSubmit, control, reset, setValue } = useForm();
+  const onSubmit = async ({ tel }) => {
+    const response = await axios.get(`?phone=${tel}`);
+    setState((prev) => {
+      const { checks } = response.data;
+      return { ...prev, data: checks, message: 'Ваш телефон не найден среди победителей' };
+    });
+    reset();
+    setValue('tel', '');
+  };
+
   return (
-    <Carousel indicators={false}>
-      {state.data.map(({ phone, draw_period, prise, number }) => (
-        <Carousel.Item key={number}>
-          <div className="mobileList">
-            <div className="mobileList__row">
-              <img src={logo} alt="" />
-              <div className="prize">{prise}</div>
-              <div className="phone-number">{phone}</div>
-              <div className="date">{draw_period}</div>
-            </div>
-          </div>
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} className="form">
+        <Controller
+          name="tel"
+          as={InputMask}
+          control={control}
+          defaultValue=""
+          mask="+7 (999) 999-99-99"
+          alwaysShowMask
+          className="form__input"
+        />
+        <button type="submit" className="form__button" />
+      </form>
+
+      {state.data.length === 0 ? (
+        <div className="list__message">{state.message}</div>
+      ) : (
+        <Carousel indicators={false}>
+          {state.data.map(({ phone, draw_period, prise, number }) => (
+            <Carousel.Item key={number}>
+              <div className="mobileList">
+                <div className="mobileList__row">
+                  <img src={logo} alt="" />
+                  <div className="prize">{prise}</div>
+                  <div className="phone-number">{phone}</div>
+                  <div className="date">{draw_period}</div>
+                </div>
+              </div>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      )}
+    </>
   );
 };
 
